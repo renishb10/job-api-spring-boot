e4c117a473d3d84862ee12abef1d841f9d5e3840
Renish B <renishb10@gmail.com>
2024-11-30 11:37:57 +0530

    initial commit

---
+++ src/test/java/com/alpstack/jobapi/JobapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpstack.jobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
